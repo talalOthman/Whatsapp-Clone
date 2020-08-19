8624e51d3a510ef05403f29513d2c6a2be60a9b9
Talal <TalalOthman2000@gmail.com>
2020-08-20 07:05:59 +0800

    added firebase database config files

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCTdLOzB_HQbzQtb5DIQGY7A_jVvXkTxgU",
+    authDomain: "whatsapp-clone-26f70.firebaseapp.com",
+    databaseURL: "https://whatsapp-clone-26f70.firebaseio.com",
+    projectId: "whatsapp-clone-26f70",
+    storageBucket: "whatsapp-clone-26f70.appspot.com",
+    messagingSenderId: "370563046725",
+    appId: "1:370563046725:web:391c5645ecb0fb34ce322c",
+    measurementId: "G-J6WNR815C1"
+  };
+  
