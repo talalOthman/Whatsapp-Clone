de429a91b8424ba2fb615131f4ad6c9dc8818a28
Talal <TalalOthman2000@gmail.com>
2020-08-20 07:50:54 +0800

    added 'IconButton' to each of the right side of the sidebar

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Sidebar.css';
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
@@ -13,9 +13,17 @@ function Sidebar() {
                 <Avatar />
             
             <div className="sidebar__headerRight">
+                <IconButton>
                 <DonutLargeIcon />
+                </IconButton>
+                
+                <IconButton>
                 <ChatIcon />
+                </IconButton>
+
+                <IconButton>
                 <MoreVertIcon />
+                </IconButton>
             </div>
 
             </div>
