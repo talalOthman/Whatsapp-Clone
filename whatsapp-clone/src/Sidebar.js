fee5660cb549cddcb25620af716dbf7b64084e5b
Talal <TalalOthman2000@gmail.com>
2020-08-21 04:40:31 +0800

    search bar added

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -4,6 +4,7 @@ import { Avatar, IconButton } from "@material-ui/core";
 import DonutLargeIcon from "@material-ui/icons/DonutLarge";
 import ChatIcon from "@material-ui/icons/Chat";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SearchOutlined } from "@material-ui/icons";
 
 
 function Sidebar() {
@@ -27,8 +28,15 @@ function Sidebar() {
             </div>
 
             </div>
+            
             <div className="sidebar__search">
+            <div className="sidebar__searchContainer">
+                <IconButton>
+                <SearchOutlined/>
+                </IconButton>
+                <input type="text" placeholder="Search or start new chat"/>
 
+            </div>
             </div>
 
             <div className="sidebar__chat">
