6b590e1c26c207575cd1c017676b9e632c8f83c3
Talal <TalalOthman2000@gmail.com>
2020-08-21 07:06:35 +0800

    made the footer functional and desgined accordingly

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -1,11 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import { Avatar, IconButton } from "@material-ui/core";
 import { AttachFile, MoreVert, SearchOutlined} from "@material-ui/icons";
+import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
+import MicIcon from "@material-ui/icons/Mic";
 
 import "./Chat.css";
 
 function Chat() {
     const [seed, setSeed] = useState('');
+    const [input, setInput] = useState('');
+
+    const sendMessage = (e) =>{
+
+    }
 
     useEffect(() => {
         return () => {
@@ -43,7 +50,12 @@ function Chat() {
                 
             </div>
             <div className="chat__footer">
-                
+                <InsertEmoticonIcon />
+                <form>
+                    <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Type a message"/>
+                    <button onClick={sendMessage} type="submit">Send a message</button>
+                </form>
+                <MicIcon />
             </div>
         </div>
     )
