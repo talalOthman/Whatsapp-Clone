4db0a3aa81b6ae54b7b0875b2751e447b8aafbbf
Talal <TalalOthman2000@gmail.com>
2020-08-23 06:30:30 +0800

    added google authentication for users to sign in with

--- whatsapp-clone/src/App.js
+++ whatsapp-clone/src/App.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-
+import Login from './Login';
 
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
+    
     <div className="app">
-      <div className="app__body">
+      {!user ? (
+        <Login/>
+      ): (
+        <div className="app__body">
         <Router>
         <Sidebar />
-        
+
           <Switch>
             <Route path="/rooms/:roomId">
             <Chat />
@@ -24,6 +30,7 @@ function App() {
 
         </Router>
       </div>
+      )}
     </div>
   );
 }
