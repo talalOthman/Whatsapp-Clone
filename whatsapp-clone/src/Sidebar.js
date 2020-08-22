956c38dece3f3ccdfd01c88821cadd1fd7a4c7fa
Talal <TalalOthman2000@gmail.com>
2020-08-23 07:58:48 +0800

    user's photo is fetched from google

--- whatsapp-clone/src/Sidebar.js
+++ whatsapp-clone/src/Sidebar.js
@@ -7,10 +7,12 @@ import MoreVertIcon from "@material-ui/icons/MoreVert";
 import { SearchOutlined } from "@material-ui/icons";
 import SidebarChat from './SidbarChat';
 import db from './firebase';
+import { useStateValue } from './StateProvider';
 
 
 function Sidebar() {
     const [rooms, setRooms] = useState([]);
+    const [{ user }, dispatch] = useStateValue();
 
     useEffect(() => {
         const remove = db.collection('rooms').onSnapshot(snapshot => (
@@ -29,7 +31,7 @@ function Sidebar() {
     return (
         <div className="sidebar">
             <div className="sidebar__header">
-                <Avatar />
+                <Avatar src={user?.photoURL}/>
 
                 <div className="sidebar__headerRight">
                     <IconButton>
