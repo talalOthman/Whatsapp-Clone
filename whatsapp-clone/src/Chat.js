5a65063468de474082973884c4567ec942a40524
Talal <TalalOthman2000@gmail.com>
2020-08-23 08:18:40 +0800

    messages have it's own collection from each chat room

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Avatar, IconButton } from "@material-ui/core";
-import { AttachFile, MoreVert, SearchOutlined} from "@material-ui/icons";
+import { AttachFile, MoreVert, SearchOutlined } from "@material-ui/icons";
 import InsertEmoticonIcon from "@material-ui/icons/InsertEmoticon";
 import MicIcon from "@material-ui/icons/Mic";
 import { useParams } from "react-router-dom";
@@ -13,20 +13,27 @@ function Chat() {
     const [input, setInput] = useState('');
     const { roomId } = useParams();
     const [roomName, setRoomName] = useState('');
+    const [messages, setMessages] = useState([]);
 
-    const sendMessage = (e) =>{
+    const sendMessage = (e) => {
         e.preventDefault();
         console.log(input);
         setInput('');
 
     };
 
-    useEffect(() =>{
-        if(roomId){
+    useEffect(() => {
+        if (roomId) {
             db.collection("rooms")
-            .doc(roomId)
-            .onSnapshot((snapshot) => setRoomName
-            (snapshot.data().name));
+                .doc(roomId)
+                .onSnapshot((snapshot) => setRoomName
+                    (snapshot.data().name));
+
+            db.collection("rooms").doc(roomId).collection("messages")
+                .orderBy('timestamp', 'asc').onSnapshot(snapshot => (
+                    setMessages(snapshot.docs.map(doc =>
+                        doc.data()))
+                ));
         }
     }, [roomId]);
 
@@ -38,7 +45,7 @@ function Chat() {
     return (
         <div className="chat">
             <div className="chat__header">
-                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
 
                 <div className="chat__headerInfo">
                     <h3>{roomName}</h3>
@@ -58,17 +65,22 @@ function Chat() {
                 </div>
             </div>
             <div className="chat__body">
-                <p className={`chat__message ${true && `chat__reciever`}`}>
-                <span className="chat__name">Talal Othman</span>
-                    Hey
-                <span className="chat__timestamp">3:53pm</span>
-                </p>
-                
+                {messages.map((message) => (
+                    <p className={`chat__message ${true && `chat__reciever`}`}>
+                        <span className="chat__name">{message.name}</span>
+                        {message.message}
+                        <span className="chat__timestamp">
+                            {new Date(message.timestamp?.toDate()).toUTCString()}
+                        </span>
+                    </p>
+                ))}
+
+
             </div>
             <div className="chat__footer">
                 <InsertEmoticonIcon />
                 <form>
-                    <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Type a message"/>
+                    <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder="Type a message" />
                     <button onClick={sendMessage} type="submit">Send a message</button>
                 </form>
                 <MicIcon />
