b96b562c5bef5402e322da13b6b29b61630c21f2
Talal <TalalOthman2000@gmail.com>
2020-08-25 02:32:44 +0800

    'last message' functionality is added

--- whatsapp-clone/src/SidbarChat.js
+++ whatsapp-clone/src/SidbarChat.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 
 function SidbarChat({ addNewChat, id, name }) {
     const [seed, setSeed] = useState('');
+    const [messages, setMessages] = useState('');
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
@@ -14,29 +15,39 @@ function SidbarChat({ addNewChat, id, name }) {
     const createChat = () => {
         const roomName = prompt("Please enter name for chat");
 
-        if (roomName){
+        if (roomName) {
             db.collection('rooms').add({
                 name: roomName,
             });
         }
     };
 
-    return !addNewChat ?(
+    useEffect(() => {
+        if (id) {
+            db.collection("rooms").doc(id).collection("messages")
+                .orderBy("timestamp", "desc").onSnapshot((snapshot) =>
+                    setMessages(snapshot.docs.map((doc) =>
+                        doc.data()
+                    )));
+        }
+    })
+
+    return !addNewChat ? (
 
         <Link to={`/rooms/${id}`}>
-        <div className="sidebarChat">
-            <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
-            <div className="sidebarChat__info">
-                <h2>{name}</h2>
-                <p>Last message...</p>
+            <div className="sidebarChat">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+                <div className="sidebarChat__info">
+                    <h2>{name}</h2>
+                    <p>{messages[0]?.message}</p>
+                </div>
             </div>
-        </div>
         </Link>
-    ):(
-        <div className="sidebarChat" onClick={createChat}>
-            <h2>Add new Chat</h2>
-        </div>
-    );
+    ) : (
+            <div className="sidebarChat" onClick={createChat}>
+                <h2>Add new Chat</h2>
+            </div>
+        );
 }
 
 export default SidbarChat
